1a80c3a1e6119281dc0708ddf900fd7a66be4358
R2DWess <gusterlima@hotmail.com>
2024-09-03 22:03:47 -0300

    Fisrt commit

---
+++ src/main/java/com/email/uber/wzzy/WzzyApplication.java
@@ -0,0 +1,13 @@
+package com.email.uber.wzzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WzzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WzzyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/email/uber/wzzy/WzzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.uber.wzzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WzzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
