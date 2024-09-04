9f422870c683d99b2502606b560507725ca02d41
R2DWess <gusterlima@hotmail.com>
2024-09-03 22:29:26 -0300

    feat new method for config

--- src/main/java/com/email/uber/wzzy/service/EmailSenderService.java
+++ src/main/java/com/email/uber/wzzy/service/EmailSenderService.java
@@ -3,10 +3,20 @@ package com.email.uber.wzzy.service;
 import com.email.uber.wzzy.adapters.EmailSenderGateway;
 import com.email.uber.wzzy.core.EmailSenderUserCase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailSenderService implements EmailSenderUserCase {
+
+    @Value("${aws.accessKeyId}")
+    private String accessKeyId;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
+    @Value("${aws.region}")
+    private String region;
 
     private final EmailSenderGateway emailSenderGateway;
 
